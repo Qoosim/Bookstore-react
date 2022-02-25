d90341ef557c3f743f47e774a73b6e8eb02b32b4
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-02-25 01:47:14 +0100

    Refactor the project to add and remove books to and from the remote server- api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,11 +1,17 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOKS = 'bookStore/books/FETCH_BOOKS';
 
 const initialState = [];
 
 export const addBook = (newBook) => ({
   type: ADD_BOOK,
   payload: newBook,
+});
+
+export const getBooks = (books) => ({
+  type: GET_BOOKS,
+  payload: books,
 });
 
 export const removeBook = (id) => ({
@@ -15,13 +21,15 @@ export const removeBook = (id) => ({
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_BOOKS:
+      return action.payload;
     case ADD_BOOK:
       return [
         ...state,
         action.payload,
       ];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.item_id !== action.payload);
     default:
       return state;
   }
