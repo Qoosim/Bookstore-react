a25c0eb5555ba7013b363d735d78cb0201f4cf77
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-02-25 22:08:03 +0100

    Setup tailwindcss

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -13,7 +13,7 @@ const BookList = () => {
 
   return (
     <div className="App">
-      <h3>Book Lists</h3>
+      <h3 className="text-3xl font-bold text-yellow bg-blue-400">Book Lists</h3>
       <div>
         {
           books.map((book) => (
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable global-require */
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      spacing: {
+        lg: '10vw',
+        sm: '3vw',
+        halfHeight: '50vh',
+      },
+      colors: {
+        primary: '#0290ff',
+      },
+    },
+  },
+  plugins: [],
+};
